c44e4c51efabb0cf7af0521c1d1c61e4da70d80c
Miyoyo <miyoyo@live.be>
2019-07-31 16:22:23 +0200

    Test now compares the entire segments, instead of just one

--- lib/core/word_binary.dart
+++ lib/core/word_binary.dart
@@ -21,179 +21,38 @@ class WordBinary {
 
     if (other.wordLen + n < wordLen) return false;
 
-    for (int i = 0; i < Dictionary.NUM_INT; i++) {
-      n = _test(segments[i], other.segments[i], n);
-
-      if (n == -1) return false;
-    }
-
-    return true;
+    return _test(other.segments, numBlanks);
   }
 
-  int _test(int sA, int sB, int numBlanks) {
-    var n = numBlanks;
+  bool _test(Uint8List other, int numBlanks) {
+    var _availableBlanks = numBlanks;
 
-    if (sA == 0) return n;
+    // Otherwise, check every letter
+    for (var i = 1; i < 27; i++) {
+      final _thisLetter = segments[i];
+      final _otherLetter = other[i];
 
-    int M = (sB ^ sA) & sA;
+      // Mask off the bits of the letters we currently have
+      // This leaves only N consecutive bits when we're missing N letters
+      var _missingLetters = (_thisLetter ^ _otherLetter) & _otherLetter;
 
-    if (M == 0)
-      return n;
-    else if (n == 0) return -1;
+      // We have enough of this letter
+      if (_missingLetters == 0) continue;
 
-    for (int N; M > 0; M >>= 7) {
-      N = M & 0x7F;
-
-      if (N > 0) {
-        switch (N) {
-          case 1:
-          case 2:
-          case 4:
-          case 8:
-          case 16:
-          case 32:
-          case 64:
-            n -= 1;
-            break;
-          case 3:
-          case 5:
-          case 6:
-          case 9:
-          case 10:
-          case 12:
-          case 17:
-          case 18:
-          case 20:
-          case 24:
-          case 33:
-          case 34:
-          case 36:
-          case 40:
-          case 48:
-          case 65:
-          case 66:
-          case 68:
-          case 72:
-          case 80:
-          case 96:
-            n -= 2;
-            break;
-          case 7:
-          case 11:
-          case 13:
-          case 14:
-          case 19:
-          case 21:
-          case 22:
-          case 25:
-          case 26:
-          case 28:
-          case 35:
-          case 37:
-          case 38:
-          case 41:
-          case 42:
-          case 44:
-          case 49:
-          case 50:
-          case 52:
-          case 56:
-          case 67:
-          case 69:
-          case 70:
-          case 73:
-          case 74:
-          case 76:
-          case 81:
-          case 82:
-          case 84:
-          case 88:
-          case 97:
-          case 98:
-          case 100:
-          case 104:
-          case 112:
-            n -= 3;
-            break;
-          case 15:
-          case 23:
-          case 27:
-          case 29:
-          case 30:
-          case 39:
-          case 43:
-          case 45:
-          case 46:
-          case 51:
-          case 53:
-          case 54:
-          case 57:
-          case 58:
-          case 60:
-          case 71:
-          case 75:
-          case 77:
-          case 78:
-          case 83:
-          case 85:
-          case 86:
-          case 89:
-          case 90:
-          case 92:
-          case 99:
-          case 101:
-          case 102:
-          case 105:
-          case 106:
-          case 108:
-          case 113:
-          case 114:
-          case 116:
-          case 120:
-            n -= 4;
-            break;
-          case 31:
-          case 47:
-          case 55:
-          case 59:
-          case 61:
-          case 62:
-          case 79:
-          case 87:
-          case 91:
-          case 93:
-          case 94:
-          case 103:
-          case 107:
-          case 109:
-          case 110:
-          case 115:
-          case 117:
-          case 118:
-          case 121:
-          case 122:
-          case 124:
-            n -= 5;
-            break;
-          case 63:
-          case 95:
-          case 111:
-          case 119:
-          case 123:
-          case 125:
-          case 126:
-            n -= 6;
-            break;
-          case 127:
-            n -= 7;
-            break;
-        }
-
-        if (n < 0) return -1;
+      // Here we assume that we will never encounter non-consecutive bits
+      while (_missingLetters & 0x1 == 0) {
+        _missingLetters = _missingLetters >> 1;
       }
+
+      // Use wildcards to replace the missing letters
+      _availableBlanks -= _missingLetters;
+
+      // If we can't, give up
+      if (_availableBlanks < 0) return false;
     }
 
-    return n;
+    // We have enough letters for everything
+    return true;
   }
 
   Uint8List toBinary() {
